a3073878a8e49ef566fecc5be885819c2edbdc99
nat <natalya.koita08@gmail.com>
2021-08-02 02:11:21 +0300

    task08_1

--- lesson_06/task_3/main.go
+++ lesson_06/task_3/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,6 +12,19 @@ import (
 
 type response struct {
 	Cookie []string
+}
+
+type Request struct {
+	Token     string    `json:"token"`
+	CreatedAt time.Time `json:"createdAt"`
+	ExpiredAt time.Time `json:"expiredAt"`
+}
+
+func (r *Request) Prepare(token string) {
+	r.Token = token
+	now := time.Now()
+	r.CreatedAt = now
+	r.ExpiredAt = now.Add(120 * time.Second)
 }
 
 func handlerCookie(w http.ResponseWriter, r *http.Request) {
@@ -42,12 +57,33 @@ func handlerCookie(w http.ResponseWriter, r *http.Request) {
 		// set cookie
 		cookie := http.Cookie{Name: name, Value: address, Expires: expiration}
 		http.SetCookie(w, &cookie)
+
+		err := sendRequest(cookie.String())
+		if err != nil {
+			fmt.Fprintf(w, "Sendrequest err: %v", err)
+			return
+		}
+
 		// redirect data to "/"
 		http.Redirect(w, r, "http://localhost:8083/", http.StatusSeeOther)
 
 	default:
 		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
 	}
+}
+
+func sendRequest(token string) error {
+	req := new(Request)
+	req.Prepare(token)
+	json_date, err := json.Marshal(req)
+	if err != nil {
+		return err
+	}
+	_, err = http.Post("http://save_token:8080/api/v0/save", "application/json", bytes.NewBuffer(json_date))
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func main() {
